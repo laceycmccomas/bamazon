76d0ffdec07f9a94675efd4be677e828d35207c6
thelaceyedit <thelaceyedit@gmail.com>
2018-09-10 11:17:55 -0400

    adding products

--- bamazon.sql
+++ bamazon.sql
@@ -1,35 +1,35 @@
-create database bamazon;
+create database bamazon_db;
 
-use bamazon;
+use bamazon_db;
 
 create table products(
 	item_id integer auto_increment not null,
-    product_name varchar(50) not null,
-    department_name varchar(50) not null,
+    product_name varchar(100) not null,
+    department_name varchar(100) not null,
     price integer not null,
     stock_quantity integer not null,
     primary key(item_id)
 );
 
 insert into products(product_name, department_name, price, stock_quantity)
-values ("banana", "produce", 2, 250);
+values ("salmon", "seafood", 1, 9000);
 insert into products(product_name, department_name, price, stock_quantity)
-values ("persimmon", "produce", 3, 110);
+values ("apple", "produce", 6, 450);
 insert into products(product_name, department_name, price, stock_quantity)
-values ("kayak", "recreation", 150, 25);
+values ("lipstick", "cosmetics", 150, 25);
 insert into products(product_name, department_name, price, stock_quantity)
-values ("gri gri", "recreation", 22, 95);
+values ("toilet paper", "household", 22, 95);
 insert into products(product_name, department_name, price, stock_quantity)
-values ("thimble", "crafts", 1, 500);
+values ("spatula", "household", 1, 500);
 insert into products(product_name, department_name, price, stock_quantity)
-values ("sewing machine", "crafts", 164, 81);
+values ("shower curtain", "household", 164, 81);
 insert into products(product_name, department_name, price, stock_quantity)
-values ("beer", "beverages", 4, 1000);
+values ("pinot noir", "beverages", 4, 1000);
 insert into products(product_name, department_name, price, stock_quantity)
-values ("wine", "beverages", 12, 875);
+values ("olives", "produce", 12, 875);
 insert into products(product_name, department_name, price, stock_quantity)
-values ("javelin", "armory", 190, 45);
+values ("espresso", "beverages", 190, 45);
 insert into products(product_name, department_name, price, stock_quantity)
-values ("shield", "armory", 275, 110);
+values ("lettuce", "produce", 275, 110);
 
 select * from products;
